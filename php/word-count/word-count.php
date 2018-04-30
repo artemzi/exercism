47184582c00513c977cda0c94656e35bfb38cd0e
Artem Zinoviev <arzinoviev@gmail.com>
2018-04-30 11:58:05 +0300

    alternative implementation

--- php/word-count/word-count.php
+++ php/word-count/word-count.php
@@ -2,15 +2,16 @@
 
 function wordCount($line)
 {
-    $line = trim(preg_replace('/[^[:alnum:][:space:]]/u', '', $line)); // remove non alpha and trailing spaces
-    $data = preg_split('/\s+/', $line); // split by whitespace
-    $result = [];
+    // $line = trim(preg_replace('/[^[:alnum:][:space:]]/u', '', $line)); // remove non alpha and trailing spaces
+    // $data = preg_split('/\s+/', $line); // split by whitespace
+    // $result = [];
 
-    foreach ($data as $val) {
-        $val = strtolower($val);
-        if (!array_key_exists($val, $result)) $result[$val] = 0;
-        $result[$val]+=1;
-    }
+    // foreach ($data as $val) {
+    //     $val = strtolower($val);
+    //     if (!array_key_exists($val, $result)) $result[$val] = 0;
+    //     $result[$val]++;
+    // }
 
-    return $result;
+    // return $result;
+    return array_count_values(str_word_count(strtolower($line), 1, '12'));
 }
